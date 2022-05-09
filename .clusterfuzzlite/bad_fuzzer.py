14c575ffdb2bedd2d28522b2e1cad845cc8ff88d
khushbureddy-gangarapu <104974373+khushbureddy-gangarapu@users.noreply.github.com>
2022-05-09 08:31:35 +0100

    Update bad_fuzzer.py

--- .clusterfuzzlite/bad_fuzzer.py
+++ .clusterfuzzlite/bad_fuzzer.py
@@ -4,6 +4,7 @@ import sys
 def TestOneInput(data):
     if data == b"bad":
         raise RuntimeError("Badness!")
-
+        
+atheris.instrument_all()
 atheris.Setup(sys.argv, TestOneInput)
 atheris.Fuzz()
